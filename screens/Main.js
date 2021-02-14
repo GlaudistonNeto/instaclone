f56ea66a424ce50d42bf06bf62fe263754e1a5fc
gneto <glaudistonson@gmail.com>
2021-02-14 15:37:29 -0300

    added the navigation to the and into mainscreen

--- screens/Main.js
+++ screens/Main.js
@@ -1,38 +1,62 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { createMaterialBottomTabNavigator }
+from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchUser } from '../redux/actions/index';
+
+import FeedScreen from './main/Feed';
+import AddScreen from './main/Add';
+import ProfileScreen from './main/Profile';
+
+const Tab = createMaterialBottomTabNavigator();
 
 export class Main extends Component {
   componentDidMount() {
     this.props.fetchUser();
   }
   render() {
-    const { currentUser } = this.props;
-
-    if (currentUser === undefined) {
-      return (
-        <View />
-      );
-    };
     return (
-      <View style={styles.loadingStyle}>
-          <Text>{currentUser.name} is logged In</Text>
-      </View>
+      <Tab.Navigator>
+        <Tab.Screen name='Feed' component={FeedScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name='home'
+                color={color}
+                size={26}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen name='Add' component={AddScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name='home'
+                color={color}
+                size={26}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen name='Profile' component={ProfileScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name='home'
+                color={color}
+                size={26}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  loadingStyle: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#97a356'
-  }
-});
 
 const mapStateToProps = (store) => ({
   currentUser: store.userState.currentUser
