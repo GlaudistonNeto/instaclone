cf3679109c3aee08fa3ebbcd6368007eda3f509e
gneto <glaudistonson@gmail.com>
2021-02-14 22:45:19 -0300

    userProfile showing images

--- screens/main/Profile.js
+++ screens/main/Profile.js
@@ -1,18 +1,61 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 
-export default function Profile() {
+import { connect } from 'react-redux';
+
+function Profile(props) {
+  const { currentUser, posts } = props;
+  console.log({ currentUser, posts });
   return (
     <View style={styles.container}>
-      <Text>Profile Screen</Text>
+      <View style={styles.infoContainer}>
+        <Text>{currentUser.name}</Text>
+        <Text>{currentUser.email}</Text>
+      </View>
+
+      <View style={styles.galleryContainer}>
+        <FlatList
+          numColumns={3}
+          horizontal={false}
+          data={posts}
+          renderItem={({item}) => (
+            <View style={styles.imageContainer}>
+              <Image
+                style={styles.image}
+                source={{ uri: item.downloadURL }}
+              />
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
 };
 
+const mapStateToProps = (store) => ({
+  currentUser: store.userState.currentUser,
+  posts: store.userState.posts
+});
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
+    marginTop: 40,
+  },
+  infoContainer: {
+    margin: 10,
+  },
+  galleryContainer: {
+    flex: 1,
+  },
+  imageContainer: {
+    flex: 1/1
+  },
+  image: {
+    flex: 1,
+    aspectRatio: 1/3
+  },
 });
+
+export default connect(mapStateToProps, null)(Profile);
